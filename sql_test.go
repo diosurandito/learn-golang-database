db44c44c91d67672c5dd0543a663b52768a54c6c
diosurandito <diosurandito@gmail.com>
2021-12-05 10:01:39 +0700

    10.SQL dengan Parameter

--- sql_test.go
+++ sql_test.go
@@ -113,3 +113,47 @@ func TestSqlInjection(t *testing.T) {
 		fmt.Println("Gagal login")
 	}
 }
+
+func TestSqlInjectionSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	username := "admin'; #"
+	password := "salah"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses login", username)
+	} else {
+		fmt.Println("Gagal login")
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	username := "dio'; Drop table user; #"
+	password := "dio"
+	script := "INSERT INTO user(username, password) VALUES(?, ?)"
+	_, err := db.ExecContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new user")
+}
